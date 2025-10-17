b43e17d6532840cc5f4860053e948c909c876e1e
Barnabas Affonshike <117881010+Barnabas001@users.noreply.github.com>
2025-10-17 15:21:38 +0100

    Simplify time update to display current timestamp

--- script.js
+++ script.js
@@ -1,11 +1,4 @@
-const curTime = document.querySelector('[data-testid="test-user-time"]');
+document.querySelector('[data-testid="test-user-time"]').textContent = Date.now()
 
-function updateTime() {
-  const now = new Date();
-  const hours = String(now.getHours()).padStart(2, "0");
-  const minutes = String(now.getMinutes()).padStart(2, "0");
-  const seconds = String(now.getSeconds()).padStart(2, "0");
-  curTime.textContent = `${hours}:${minutes}:${seconds}`;
-}
-setInterval(updateTime, 1000);
-updateTime();
+
+
